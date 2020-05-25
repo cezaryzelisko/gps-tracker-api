96d1d2958dcbe0d9238c8dd5f7c74a327f779465
Cezary Żelisko <cezary.zelisko@gmail.com>
2020-05-25 16:59:07 +0200

    initial API that receives GPS footprint

---
+++ gps_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GpsApiConfig(AppConfig):
+    name = 'gps_api'
---
+++ gps_api/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+
+@ensure_csrf_cookie
+def index(request):
+    return HttpResponse('GPS footprint received!')
